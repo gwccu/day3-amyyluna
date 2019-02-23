675ccea834d4d11987434c0f001cbe92fb7ba16d
amyyluna <afrancis@beaconschool.org>
2019-02-23 17:31:40 +0000

    i worked on strings

--- problemSetDay3.py
+++ problemSetDay3.py
@@ -1,1 +1,10 @@
 # File name: problemSetDay3.py
+answer = input("I have a tail and large ears," + " what animal am I? ")
+if answer == str('dinosaur'):
+    print('I am not a dinosaur')
+elif answer == str('dog'):
+    print('I am not a dog')
+elif answer == str('elephant'):
+    print('Great Job!' + ' You guessed what I am!')
+# File name: problemSetDay3.py2
+i = int(input(''))
